5b109914b37de1ac00a067e9b4063540b67d3456
Kurt Funai <jesuiskurt@gmail.com>
2012-10-20 01:21:24 -0400

    Updated navbar and devise views

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :list
-  attr_accessible :description, :name, :url
+  attr_accessible :description, :name, :url, :priority
   validates :name, :presence => true
 end
