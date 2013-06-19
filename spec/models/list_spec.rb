8472978267a14572d390e5246d860eba72bfe3f3
Kurt Funai <jesuiskurt@gmail.com>
2013-06-18 23:36:26 -0400

    Added test for list model

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,9 +2,30 @@ require 'spec_helper'
 
 describe "list" do
   context "validation" do
+    before(:each) do
+      @list = create(:list)
+    end
     it "creates valid instances" do
-      list = FactoryGirl.build :list
-      list.should be_valid
+      @list.should be_valid
+    end
+
+    it "was found using friendly id" do
+      @list.found_using_friendly_id?(@list.slug).should eq(true)
+    end
+
+    it "was not found using friendly id" do
+      @list.found_using_friendly_id?(@list.id).should eq(false)
+    end
+
+    it "generates friendly id for new records" do
+      list = build :list
+      list.should_generate_new_friendly_id?.should eq(true)
+    end
+
+    it "creates a slug for new list" do
+      list = build :list
+      list.date_and_user_id
+      list.slug.should_not be_nil
     end
   end
 end
